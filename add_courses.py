ffed2fe1f0be522239a5050c4653846d85493131
Max Terrae Dickens <47918604+MTDickens@users.noreply.github.com>
2023-09-17 18:43:46 +0800

    Update add_courses.py

--- add_courses.py
+++ add_courses.py
@@ -4,45 +4,53 @@ from gcsa.event import Event, date, datetime
 from gcsa.google_calendar import GoogleCalendar
 import chinese_calendar
 
-my_email = "mtdickens1998@gmail.com"
-calendar = GoogleCalendar(my_email)
 
-# Summary format
-def summary_format(name, count):
-    return f"{name}: Lec {count}"
+def main():
 
-## Course information
-course_name = "My course"
+    my_email = "mtdickens1998@gmail.com"
+    calendar = GoogleCalendar(my_email)
 
-start_date = datetime.today()
-tot_count = 1  ## total count of lectures
-first_course_index = 1  ## the index of first lecture that you want to add
-duration = 2  ## duration of one lecture
+    # Summary format
+    def summary_format(name, count):
+        return f"{name}: Lec {count}"
 
-exception_days = ['Sunday']
-exception_dates = []
-exception_dates_check = lambda date: chinese_calendar.get_holiday_detail(date)[1] == None ## real-time check
-## 'None' means this date is NOT a special holiday (i.e. just normal weekday or weekend)
+    ## Course information
+    course_name = "Complex analysis"
 
-# Aliases
-day = timedelta(days=1)
-hours = timedelta(hours=duration)
+    start_date = (18/Sept/2023)[0]
+    tot_count = 35  ## total count of lectures
+    duration = 1.5  ## duration of one lecture
 
-# Main loop for adding courses
-for i in range(first_course_index, tot_count + 1):
-    ## skip the exception days/dates
-    while (start_date.strftime('%A') in exception_days
-           or start_date in exception_dates
-           or exception_dates_check(start_date)):
+    is_default_first_course_index = True ##  whether to 1 or the value below
+    first_course_index = 1  ## the index of first lecture that you want to add
+
+    exception_days = ['Sunday']
+    exception_dates = []
+    exception_dates_check = lambda date: chinese_calendar.get_holiday_detail(date)[1] != None ## real-time check
+    ## '!= None' means this date is NOT a special holiday (i.e. just normal weekday or weekend)
+
+    # Aliases
+    day = timedelta(days=1)
+    hours = timedelta(hours=duration)
+
+    # Main loop for adding courses
+    for i in range(1 if is_default_first_course_index else first_course_index, tot_count + 1):
+        ## skip the exception days/dates
+        while (start_date.strftime('%A') in exception_days
+            or start_date in exception_dates
+            or exception_dates_check(start_date)):
+            start_date += day
+
+        calendar.add_event(
+            Event(
+                summary=summary_format(course_name, i),
+                start=start_date,
+                end=start_date + hours
+            )
+        )
+
+        ## loop to the next day
         start_date += day
 
-    calendar.add_event(
-        Event(
-            summary=summary_format(course_name, i),
-            start=start_date,
-            end=start_date + hours
-        )
-    )
-
-    ## loop to the next day
-    start_date += day
+if __name__ == '__main__':
+    main()
